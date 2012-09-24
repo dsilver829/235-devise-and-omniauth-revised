5fd4270fa7bfb6334ab448701e2999d3302b14e6
David Silver <dsilver@candidatemetrics.com>
2012-09-23 20:28:35 -0400

    set up Omniauth for Twitter

--- blog/config/initializers/devise.rb
+++ blog/config/initializers/devise.rb
@@ -206,7 +206,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :twitter, ENV['N0Zut2yr9koNshhLHkPQ'], ENV['zpFE8FrrWVt9c4NqtcjuPtX6NWlcf6PaKdSYdp3w']
+  config.omniauth :twitter, 'N0Zut2yr9koNshhLHkPQ', 'zpFE8FrrWVt9c4NqtcjuPtX6NWlcf6PaKdSYdp3w'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
